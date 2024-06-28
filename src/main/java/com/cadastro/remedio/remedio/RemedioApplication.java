ce72e3b3295bad527eef05053aeed11404f72ca2
RONALDO NUNES BELEM <98597641+RonaldoBelem@users.noreply.github.com>
2024-06-28 07:33:20 -0300

    conecxao padra com obanco de dados

--- src/main/java/com/cadastro/remedio/remedio/RemedioApplication.java
+++ src/main/java/com/cadastro/remedio/remedio/RemedioApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = @Server( url = "/", description = "server raiz do sistema"))
 public class RemedioApplication {
 
 	public static void main(String[] args) {
