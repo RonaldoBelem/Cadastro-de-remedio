98ed6cd80d30a783b50db54de482a67f602c4f49
RonaldoBelem <ronaldonunes8@gmail.com>
2024-06-28 07:29:44 -0300

    conexao banco de dados

--- src/main/java/com/cadastro/remedio/remedio/RemedioApplication.java
+++ src/main/java/com/cadastro/remedio/remedio/RemedioApplication.java
@@ -1,9 +1,13 @@
 package com.cadastro.remedio.remedio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Description;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = @Server( url = "/", description = "server raiz do sistema"))
 public class RemedioApplication {
 
 	public static void main(String[] args) {
